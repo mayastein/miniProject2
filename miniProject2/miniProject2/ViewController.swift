118f35ec04d3a9141723f20a5e8f85a5343d73e2
mayastein <68654833+mayastein@users.noreply.github.com>
2020-07-23 20:23:30 -0400

    Add files via upload

---
+++ miniProject2/miniProject2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miniProject2
+//
+//  Created by Maya stein on 7/23/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var welcomePgTxt: UILabel!
+    
+    
+    @IBOutlet weak var welcomePhoto: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
